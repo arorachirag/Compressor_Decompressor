7903622893635b31bbe5449b6427af98d274a967
arorachirag <96038346+arorachirag@users.noreply.github.com>
2023-10-14 22:51:24 +0530

    Add files via upload

---
+++ Compressor_Decompressor/src/Com_Decom/compressor.java
@@ -0,0 +1,35 @@
+package compressor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+
+public class compressor {
+    public static void method(File file) throws IOException{
+        String filedirectory=file.getParent();
+
+        FileInputStream fis=new FileInputStream(file);
+        FileOutputStream fos=new FileOutputStream(filedirectory +"Compressed1.gz");
+        GZIPOutputStream gzip=new GZIPOutputStream(fos);
+
+        byte []buffer=new byte[1024];
+        int len;
+
+        while((len=fis.read(buffer))!=-1){
+            gzip.write(buffer,0,len);
+        }
+
+        gzip.close();
+        fos.close();
+        fis.close();
+
+    }
+    public static void main(String [] args) throws IOException
+    {
+        File path=new File("C:\\Users\\Dell\\Desktop\\Compressor_Decompressor\\hello.txt");
+        method(path);
+    }
+}
